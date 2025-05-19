4862a29a065fecc3ed00db90321de3166d17df5e
Duubl <147439692+Duubl@users.noreply.github.com>
2025-05-19 12:19:12 -0700

    Fixed incorrect column mapping name

--- src/main/java/com/Duubl/JobAppTracker/repository/JobApplicationRepository.java
+++ src/main/java/com/Duubl/JobAppTracker/repository/JobApplicationRepository.java
@@ -6,6 +6,8 @@ import com.Duubl.JobAppTracker.model.ApplicationStatus;
 import com.Duubl.JobAppTracker.model.JobApplication;
 import com.Duubl.JobAppTracker.model.User;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JobApplicationRepository extends JpaRepository<JobApplication, Integer> {
     List<JobApplication> findByUser(User user);
@@ -13,5 +15,6 @@ public interface JobApplicationRepository extends JpaRepository<JobApplication, 
     List<JobApplication> findByStatus(ApplicationStatus status);
     List<JobApplication> findByUserAndStatus(User user, ApplicationStatus status);
     List<JobApplication> findByUserIdAndStatus(int userId, ApplicationStatus status);
-    List<JobApplication> findByCompanyNameIgnoreCase(String companyName);
+    @Query("SELECT j FROM JobApplication j WHERE LOWER(j.company_name) = LOWER(:companyName)")
+    List<JobApplication> findByCompanyNameIgnoreCase(@Param("companyName") String companyName);
 }
