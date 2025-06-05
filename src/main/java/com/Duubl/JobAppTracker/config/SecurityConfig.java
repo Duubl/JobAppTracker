500d4dd37f0958aec47c1b607f01c31e7b08c903
Duubl <147439692+Duubl@users.noreply.github.com>
2025-06-05 09:25:04 -0700

    Added user and admin accounts

--- src/main/java/com/Duubl/JobAppTracker/config/SecurityConfig.java
+++ src/main/java/com/Duubl/JobAppTracker/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -29,13 +30,19 @@ public class SecurityConfig {
 
 	@Bean
 	public UserDetailsService userDetailsService() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		UserDetails user = users
 			.username("user")
-			.password("password")
+			.password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
 			.roles("USER")
 			.build();
+		UserDetails admin = users
+			.username("admin")
+			.password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+			.roles("USER", "ADMIN")
+			.build();
 
-		return new InMemoryUserDetailsManager(userDetails);
+		return new InMemoryUserDetailsManager(user, admin);
 	}
 
 }
